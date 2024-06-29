d4b9e0b288bdd496c7e5c3321f053d06736f3b1f
Praise <praisetoyo@gmail.com>
2024-06-29 10:06:41 +0100

    feat: set up docker

---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [],
+  migrations: [],
+  subscribers: []
+});
