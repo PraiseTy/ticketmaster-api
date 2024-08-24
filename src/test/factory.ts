c0e41d136fd41b2db5bb0f14bfcd2e64b0059a81
Praise <praisetoyo@gmail.com>
2024-08-24 14:52:35 +0100

    fix: test not closing after running

--- src/test/factory.ts
+++ src/test/factory.ts
@@ -1,15 +1,10 @@
 import { Server, createServer } from 'node:http';
-import express from 'express';
+import express, { Response } from 'express';
 import supertest from 'supertest';
-import { createConnection, Connection } from 'typeorm';
-import { testDBconfig } from './setup/jest-setup';
 import logger from '../logger';
 
-// const POSTGRES_URL = `postgres://${testDBconfig.user}:
-// ${testDBconfig.password}@localhost:${testDBconfig.port}/${testDBconfig.database}?authSource=admin`;
 export class TestFactory {
   private _app: express.Application;
-  private _connection: Connection;
   private _server: Server;
 
   public get app(): supertest.SuperTest<supertest.Test> {
@@ -21,26 +16,11 @@ export class TestFactory {
   }
 
   public async close(): Promise<void> {
-    if (this._connection) {
-      await this._connection.close();
-    }
     this._server.close();
   }
 
   private async startup() {
     try {
-      // Connect to PostgreSQL
-      this._connection = await createConnection({
-        type: 'postgres',
-        host: 'localhost',
-        port: Number.parseInt(testDBconfig.port, 10),
-        username: testDBconfig.user,
-        password: testDBconfig.password,
-        database: testDBconfig.database,
-        entities: [],
-        synchronize: true
-      });
-
       // Setup Express app
       this._app = express();
       this._app.use(express.json());
@@ -48,10 +28,15 @@ export class TestFactory {
       // Add routes and middleware here
       // this._app.use('/', routes);
       // this._app.use(errorHandler);
+      this._app.get('/', (_, res: Response) => {
+        res.send('Initial Commit');
+      });
 
-      this._server = createServer(this._app).listen(3010);
+      this._server = createServer(this._app).listen(8000, () => {
+        logger.info('Server is listening on port 8000');
+      });
 
-      logger.info('Server and database initialized');
+      logger.info('Server initialized');
     } catch (error) {
       logger.error(error);
       throw new Error(`Error starting up the server: ${error}`);
--- src/test/url.test.ts
+++ src/test/url.test.ts
@@ -1,9 +1,18 @@
-import request from 'supertest';
-import { app } from '../app';
+import { TestFactory } from './factory';
 
 describe('GET /', () => {
+  const factory = new TestFactory();
+
+  beforeEach(async () => {
+    await factory.init();
+  });
+
+  afterEach(async () => {
+    await factory.close();
+  });
+
   it('should return Initial Commit', async () => {
-    const response = await request(app).get('/');
+    const response = await factory.app.get('/');
     expect(response.text).toBe('Initial Commit');
     expect(response.status).toBe(200);
   });
